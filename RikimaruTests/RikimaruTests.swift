719fd6f06b819f3cc8ac2e13e4128ceec0ff595b
stefanie <tanlizhi@yuewen.com>
2020-07-06 17:29:28 +0800

    feat(test): add unittest for rikimaru

--- RikimaruTests/RikimaruTests.swift
+++ RikimaruTests/RikimaruTests.swift
@@ -7,28 +7,59 @@
 //
 
 import XCTest
+import UIKit
 @testable import Rikimaru
 
 class RikimaruTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  class ViewController: UIViewController {
+    
+    var touched = false
+    
+    var parameters: Parameters = []
+    
+    private let touchedView: UIView = {
+      let view = UIView()
+      
+      return view
+    }()
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.addSubview(touchedView)
+      
+      register("event") { [weak self] parameters in
+        guard let self = self else { return }
+        self.parameters = parameters
+        self.touched = true
+      }
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func viewDidAppear(_ animated: Bool) {
+      super.viewDidAppear(animated)
+      
+      touchedView.resolve("event", parameters: ["hello", "world"])
     }
+    
+  }
+  
+  private var window: UIWindow!
+  
+  private var viewController: ViewController!
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    viewController = ViewController()
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window.rootViewController = viewController
+    window.makeKeyAndVisible()
+  }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  func testViewControllerRegister() {
+    viewController.viewDidAppear(false)
+    
+    XCTAssert(viewController.touched, "testViewControllerRegister event falied")
+    XCTAssert((viewController.parameters as! [String]) == ["hello", "world"], "testViewControllerRegister parameters falied")
+  }
 
 }
